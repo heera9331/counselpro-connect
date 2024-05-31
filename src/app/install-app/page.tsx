14a128f7932599637c1eef222300245e470b40e1
heera9331 <heera9331@gmail.com>
2024-05-31 23:52:10 +0530

    install-app page added

---
+++ src/app/install-app/page.tsx
@@ -0,0 +1,12 @@
+import {Input, AwsCard, Button} from "@/components";
+
+const page = (props: any) => {
+
+    return (
+        <div>
+            <h1>Register Page</h1>
+        </div>
+    );
+}
+
+export default page;
